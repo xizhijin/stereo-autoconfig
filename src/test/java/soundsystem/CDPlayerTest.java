66c355efac72aefa9dfeda79c65ac548d7cb5dc4
eric <xizhijin@yeah.net>
2016-08-15 16:39:30 +0800

    change name

--- src/test/java/soundsystem/CDPlayerTest.java
+++ src/test/java/soundsystem/CDPlayerTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=CDPlayerConfig.class)
+@ContextConfiguration(classes=EnableAutoConfig.class)
 public class CDPlayerTest {
 
   @Rule
