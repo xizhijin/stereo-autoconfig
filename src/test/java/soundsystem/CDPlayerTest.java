6adb7d4b3b9213ad8b3e3262bdd915aecf69d357
eric <xizhijin@yeah.net>
2016-08-15 16:45:18 +0800

    rename file

--- src/test/java/soundsystem/CDPlayerTest.java
+++ src/test/java/soundsystem/CDPlayerTest.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=EnableAutoConfig.class)
+
 public class CDPlayerTest {
 
   @Rule
