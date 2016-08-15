2758448dc6ffd459d2b4a7d5a12f169f69425cfa
eric <xizhijin@yeah.net>
2016-08-15 16:54:54 +0800

    commit

--- src/test/java/soundsystem/CDPlayerTest.java
+++ src/test/java/soundsystem/CDPlayerTest.java
@@ -12,7 +12,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=EnableAutoConfig.class)
-
 public class CDPlayerTest {
 
   @Rule
